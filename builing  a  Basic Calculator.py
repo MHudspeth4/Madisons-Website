38deb11fdc159afcdad29489d4a67026587b29a7
MadisonsTest <91355728+MadisonsTest@users.noreply.github.com>
2021-09-24 20:51:04 -0500

    Add files via upload

---
+++ builing  a  Basic Calculator.py
@@ -0,0 +1,19 @@
+
+
+
+num1 = input("Enter a number: ")
+num2= input("Enter another decimal: ")
+result = int(num1) + float (num2)
+print (result)
+
+num3 = input("Enter a number: ")
+num4 = input("Enter a number: ")
+result= int(num3) + int(num4)
+print (result)
+
+num5 = input("Enter a decimal: ")
+num6 = input("Enter a decimal: ")
+result = float(num5) + float(num6)
+print (result)
+
+
